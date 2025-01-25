4516229ffa1c5ef7b023e188ea05d644de91a0cf
Paltita715 <manuel715pl@gmail.com>
2025-01-25 16:22:25 -0500

    prueba swiper devdep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel"
+import react from "@astrojs/react"
 import icon from "astro-icon"
 
 let prefixCounter = 0
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [
     tailwind(),
+    react(),
     icon({
       iconDir: "src/assets/icons",
       svgoOptions: {
